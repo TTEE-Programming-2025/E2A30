dbcbd8f4e8fb8c1a507375068499c7f2560e47ec
figaroyang <yang.figaro0507@gmail.com>
2025-05-19 11:38:50 +0800

    test title and password

---
+++ E2A30P4.cpp
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+int main(void) 
+{
+    system("chcp 65001");
+    puts("==========================================================================");
+    puts("                    Welcome to Your Personalized UI!                     ");
+    puts("|----------------------------------------------------------------------");
+    puts("                                                                       ");
+    puts("|  EEEEEEEEEEE       222222222           A           3333333     0000000.  ");
+    puts("|  E                        22          A A               33     0     0   ");
+    puts("|  E                       22          A   A              33     0     0   ");
+    puts("|  EEEEEEEEEEE            22          A     A        3333333     0     0   ");
+    puts("   EEEEEEEEEEE           22          AAAAAAAAA       3333333     0     0   ");
+    puts("|  E                    22          AAAAAAAAAAA           33     0     0   ");
+    puts("|  E                   22          A           A          33     0     0   ");
+    puts("|. EEEEEEEEEEE       22222222     A             A    3333333     0000000   ");
+    puts("                                                                       ");
+    puts("|----------------------------------------------------------------------");
+    puts("============================================================================");
+    char word[7] = "";
+    strcat(word, "F");
+    puts(word);
+    strcat(word, "i");
+    puts(word);
+    strcat(word, "g");
+    puts(word);
+    strcat(word, "a");
+    puts(word);
+    strcat(word, "r");
+    puts(word);
+    strcat(word, "o");
+    puts(word);
+
+    int password, attempts = 3;
+    while (attempts > 0) 
+    {
+        printf("enter the four digit password: ");
+        scanf("%d", &password);
+        if (password == 2025) 
+        {
+            printf("password id correct! Welcome!\n");
+            break;
+        } 
+        else 
+        {
+            printf("Wrong Password\n");
+            attempts--;
+        
+        if (attempts == 0) 
+        {
+            printf("Wrong password three times !\a");
+            return 0;
+        }
+    }
+
+    
+    }
+}
